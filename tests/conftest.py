651239d15b77a928507e2e2c5709a5dd42209c82
Daniel <gleek.dan@gmail.com>
2021-05-25 21:44:15 -0600

    check same thread = false for action tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,7 @@ from app.crud import create_user, create_todo
 SQLALCHEMY_DATABASE_URL = os.environ.get(
     "POSTGRES_URL_TEST", "sqlite:///./test_db.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 
 def get_test_db():
